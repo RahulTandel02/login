c9d9fd340bc6812aba1a8f9560c85802c725257c
Rahul Tandel <74199956+RahulTandel02@users.noreply.github.com>
2026-08-18 14:10:11 +0530

    Use environment variable for server port

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ http.createServer((req, res) => {
     if (req.url === '/run') {
         require('./api/run')(req, res);
     }
-}).listen(3000, () => {
+}).listen((process.env.PORT || 3000), () => {
     console.log('Server is running on port 3000');
 });
